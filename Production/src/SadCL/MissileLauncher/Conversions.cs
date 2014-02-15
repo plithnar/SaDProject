f046701391d0e5c44554cc7cbaacc8cc286847d4
plithnar <plithnar@gmail.com>
2014-02-14 17:50:51 -0800

    Fixed Theta calc for accurate tracking
    Only change was on line 17 of conversions.cs

--- Production/src/SadCL/MissileLauncher/Conversions.cs
+++ Production/src/SadCL/MissileLauncher/Conversions.cs
@@ -14,12 +14,12 @@ namespace MissileLauncher
         }
         public static double calcRadius(double x, double y, double z)
         {
-            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));// + Math.Pow(z, 2));
             
         }
         public static double calcTheta(double x, double y, double z)
         {
-            return radiansToDegrees(Math.Acos(z / calcRadius(x, y, z)));
+            return radiansToDegrees(Math.Atan2(z, calcRadius(x, y, z)));
         }
         public static double calcPhi(double x, double y)
         {
